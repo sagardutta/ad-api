7afcf611b16106b84acfe2c984c454cf74a8a524
Sagar Dutta <sagar_dutta@intuit.com>
2016-02-24 14:38:52 -0600

    1st commit

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json({limit: '5mb'}));
+app.use(bodyParser.urlencoded({limit: '5mb', extended: true}));
+
+var port = process.env.PORT || 3000;        // set our port
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+router.get('/', function(req, res){
+   console.log('Service GET request');
+   res.json({name:'hello world!!!'});
+});
+
+app.use('/api', router);
+app.listen(port);
+console.log("app started on:"+ port);
