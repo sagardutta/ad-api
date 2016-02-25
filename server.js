9ce2ebd8876233410a3f812e9b9814306cacfe60
Sagar Dutta <sagar_dutta@intuit.com>
2016-02-25 10:08:35 -0600

    Adding mongoose and mongolab addon

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+
 
 var bodyParser = require('body-parser');
+
+
+
+mongoose.connect(process.ENV.MONGOLAB_URI); 
 
 app.use(bodyParser.json({limit: '5mb'}));
 app.use(bodyParser.urlencoded({limit: '5mb', extended: true}));
@@ -11,6 +17,8 @@ var port = process.env.PORT || 3000;        // set our port
 // ROUTES FOR OUR API
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
+
+
 
 router.get('/', function(req, res){
    console.log('Service GET request');
