a52b25900ee11eee2a0387f244a651bf6dc5c851
Sagar Dutta <sagar_dutta@intuit.com>
2016-03-22 17:44:46 -0500

    adding PUT to access-control-allow-methods

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ app.use(extractRequest);
 
 app.all('/*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "*");
   res.header("Access-Control-Allow-Headers","Content-Type");
   next();
  });
